22c1a1f9549b4587327787bdd5b2e478d0cb72e7
garfel10 <garche2001@gmail.com>
2023-05-02 12:08:26 +0200

    Actualización 2/5/2023

--- Ajedrez/Fichas.cpp
+++ Ajedrez/Fichas.cpp
@@ -4,19 +4,17 @@ Fichas::Fichas(Nombrefichas tipo, int fila, int columna, bool color) {
 	tipo = Tipo;
 	fila = Fila;
 	columna = Columna;
-	color = Color;
+	color = Color; //1 para blanco 0 para negro
 }
 
 void Fichas::movimiento(int nueva_fila, int nueva_columna) {
 	bool comprobacion;
-	Nombrefichas type;
+	Nombrefichas type = Tipo;
 	int fila_actual = getFila(), columna_actual = getColumna();
 	comprobacion = restricciones(nueva_fila, nueva_columna, fila_actual, columna_actual, type);
 	if (comprobacion) {
-		nueva_fila = Fila;
-		nueva_columna = Columna;
+		setPos(nueva_fila, nueva_columna);
 	}
-
 }
 
 int Fichas::getFila() {
@@ -30,11 +28,13 @@ Nombrefichas Fichas::getFicha() {
 	return Tipo;
 }
 
-void Fichas::setPos(int fila1, int columna1, int fila2, int columna2) {
-	
+void Fichas::setPos(int fila, int columna) {
+	fila = Fila;
+	columna = Columna;
 }
 
 bool Fichas::restricciones(int nueva_fila, int nueva_columna,int fila_actual, int columna_actual, Nombrefichas t){
+	//En esta funcion se comprueba si es posible el movimiento de las fichas
 	switch (t) {
 	case Nombrefichas::PEON: //FALTARÍA METER LAS POSIBILIDADES DE COMER OTRA FICHA
 		if (nueva_fila - fila_actual <=2 && nueva_columna == columna_actual)
@@ -50,14 +50,24 @@ bool Fichas::restricciones(int nueva_fila, int nueva_columna,int fila_actual, in
 	case Nombrefichas::TORRE:
 		if ((nueva_columna == columna_actual) || (nueva_fila == fila_actual))
 			return 1;
+		else return 0;
 		break;
 	case Nombrefichas::CABALLO: //REVISAR
+		if ((((nueva_columna - columna_actual) == 1) || ((nueva_columna - columna_actual) == -1)) && (((nueva_fila - fila_actual) == 2) || (nueva_fila - fila_actual) == -2))
+			return 1;
+		if ((((nueva_fila - fila_actual) == 1) || ((nueva_fila - fila_actual) == -1)) && (((nueva_columna - columna_actual) == 2) || (nueva_columna - columna_actual) == -2))
+			return 1;
+		else return 0;
 		break;
 	case Nombrefichas::REINA:
 		if ((nueva_columna - columna_actual) == (nueva_fila - fila_actual) || ((nueva_columna == columna_actual) || (nueva_fila == fila_actual)))
 			return 1;
-
-
-
+		else return 0;
+		break;
+	case Nombrefichas::REY:
+		if (((nueva_columna - columna_actual) == 1 || (nueva_fila - fila_actual) == 1) && ((nueva_columna - columna_actual) == (nueva_fila - fila_actual) || ((nueva_columna == columna_actual) || (nueva_fila == fila_actual))))
+			return 1;
+		else return 0;
+		break;
 	}
 }
--- Ajedrez/Fichas.h
+++ Ajedrez/Fichas.h
@@ -9,7 +9,7 @@ private:
 public:
 	Fichas(Nombrefichas tipo, int fila, int columna, bool color);
 	void movimiento(int, int);
-	void setPos(int fila1, int columna1, int fila2, int columna2);
+	void setPos(int fila2, int columna2);
 	Nombrefichas getFicha();
 	int getFila();
 	int getColumna();
