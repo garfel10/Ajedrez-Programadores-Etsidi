604c20cd64bd64f143fc42e5230462b5f1150bcb
luciaaantolin <118108413+luciaaantolin@users.noreply.github.com>
2023-05-15 21:41:23 +0200

    MenuInicio.h

--- Ajedrez/MenuInicio.h
+++ Ajedrez/MenuInicio.h
@@ -2,10 +2,14 @@
 #include <ETSIDI.h>
 using ETSIDI::Sprite;
 
-enum Estado { START, PLAY };
+enum Estado { START, MENUINICIO, INFO, MENU, PLAY };
 
 class MenuInicio {
 	Sprite* menuini = new Sprite("imagenes/menu.png", 0.0, 8.0, 35.0, 35.0);
+	Sprite* info = new Sprite("imagenes/menu1.png", 0.0, 6.0, 35.0, 35.0);
+	Sprite* ins = new Sprite("imagenes/instrucciones.png", 0.0, 6.0, 35.0, 35.0);
+	Sprite* menu = new Sprite("imagenes/menu2.png", 0.0, 6.0, 35.0, 35.0);
+	
 	Estado estado;
 	float tiempo;
 	bool audio;
