04978c0af6df35b2f5e72a062f92660969b04695
luciaaantolin <118108413+luciaaantolin@users.noreply.github.com>
2023-05-15 21:44:09 +0200

    MenuInicio.cpp

--- Ajedrez/MenuInicio.cpp
+++ Ajedrez/MenuInicio.cpp
@@ -13,6 +13,14 @@ MenuInicio::MenuInicio() {
 	menuini->setSize(35.0, 35.0);
 	menuini->setPos(0.0, 8.0);
 	
+	info->setSize(35.0, 35.0);
+	info->setPos(0.0, 6.0);	
+
+	ins->setSize(35.0, 35.0);
+	ins->setPos(0.0, 7.0);
+
+	menu->setSize(35.0, 35.0);
+	menu->setPos(0.0, 6.0);	
 }
 
 MenuInicio::~MenuInicio() {
@@ -21,13 +29,39 @@ MenuInicio::~MenuInicio() {
 void MenuInicio::tecla(unsigned char key) {
 	if (estado == START)
 	{
+		if (key == 32) {
+			tiempo = 0;
+			estado = MENUINICIO;//MENU PRINCIPAL
+		}
+	}
+	if (estado == MENUINICIO) {
+		if (key == 'i' || key == 'I') {
+			tiempo = 0;
+			estado = INFO;//INSTRUCCIONES AJEDREZ
+		}
+		if (key == 13) {
+			tiempo = 0;
+			estado = MENU;//PULSE ENTER PARA COMENZAR LA PARTIDA
+		}
+	}
+	if (estado == INFO){
+		if (key == 'b' || key == 'B') {
+			tiempo = 0;
+			estado = MENUINICIO;//VOLVER AL MENU
+		}
+		if (key == 'm' || key == 'M') {
+			tiempo = 0;
+			//estado = ;//CONSULTAR LOS MOVIMIENTOS DE LAS PIEZAS
+		}
+	}
+	if (estado == MENU) {
 		if (key == 's' || key == 'S')
 		{
 			tiempo = 0;
 			estado = PLAY;
 		}
 		if (key == 'e' || key == 'E')
-			exit(0);
+			exit(0); 
 	}
 }
 
@@ -39,10 +73,30 @@ void MenuInicio::dibuja() {
 			0.0, 1.0, 0.0);
 
 		menuini->draw();
-
 		ETSIDI::playMusica("musica/musicamenu.mp3", true);
+
 	}
-	
+	else if (estado == MENUINICIO) {
+		gluLookAt(0, 6.5, 30,
+			0.0, 6.5, 0.0,
+			0.0, 1.0, 0.0);
+
+		info->draw();
+	}
+	else if (estado == INFO) {
+		gluLookAt(0, 6.5, 30,
+			0.0, 6.5, 0.0,
+			0.0, 1.0, 0.0);
+
+		ins->draw();
+	}
+	else if (estado == MENU) {
+		gluLookAt(0, 6.5, 30,
+			0.0, 6.5, 0.0,
+			0.0, 1.0, 0.0);
+
+		menu->draw();
+	}
 	else if (estado == PLAY) {
 
 		gluLookAt(5.5, 5.5, 12,
