84a56419b4449faa59ea2669840e9939f905193d
jotapacio <61844854+jotapacio@users.noreply.github.com>
2023-05-24 11:48:12 +0200

    Update casilla.h

--- Ajedrez/casilla.h
+++ Ajedrez/casilla.h
@@ -11,7 +11,7 @@ public:
 	//las casillas ocupadas seran de la 1 a la 16 y de la 48 a la 
 	unsigned char fcolor;//b black , w white. el color de la ficha se lo pasa la propia ficha. 
 	char ficha;//alfil:'A', torre 'T', caballo 'C', peon 'P', rey'K', reina 'Q', nulo '0'
-
+	int numero;//la n, el numero de casilla para devolver.
 
 
 	//a partir de aquí orientamos a coordenadas de los centros sobre las que se colocarán las piezas
