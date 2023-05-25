888cc5ed5b602c3ae56978cb1cb2df9c88f2f8b3
luciaaantolin <118108413+luciaaantolin@users.noreply.github.com>
2023-05-25 12:50:09 +0200

    Tablero.cpp

--- Ajedrez/Tablero.cpp
+++ Ajedrez/Tablero.cpp
@@ -8,7 +8,7 @@ void Tablero::dibuja(){
         {
             if ((i + j) % 2 == 0) {
                 glDisable(GL_LIGHTING);
-                glColor3ub(253, 253, 180); //marron claro
+                glColor3ub(128, 64, 0); //marron oscuro
                 glBegin(GL_POLYGON);
                 glVertex3f(i + 1.5, j + 1.5, 0);
                 glVertex3f(i + 1.5, j + 2.5, 0);
@@ -17,7 +17,7 @@ void Tablero::dibuja(){
                 glEnd();
             }
             else {
-                glColor3ub(150, 55, 0); //marron oscuro 
+                glColor3ub(253, 253, 180); //marron claro 
                 glBegin(GL_POLYGON);
                 glVertex3f(i + 1.5, j + 1.5, 0);
                 glVertex3f(i + 1.5, j + 2.5, 0);
