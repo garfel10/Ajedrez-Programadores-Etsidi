421921396b986cc478b59f942a0cc3e80cd95146
pmartinezconde <61211218+pmartinezconde@users.noreply.github.com>
2023-05-26 13:52:42 +0200

    arreglado .obj de casillero

--- Ajedrez/casilla.h
+++ Ajedrez/casilla.h
@@ -14,7 +14,6 @@ public:
 	int c; //COLUMNA (1-8)
 	int f; //FILA (1-8)
 	unsigned char color;//b black, w white
-	void casillero(); //Para generar el tablero lógico. Vector casillas de 1 a 64. cada una con su color.
 	int estado = 0; // 1 ocupada, 0 libre. Lo inicializamos a 0 y cuando se inicialicen las fichas ya se ponen a 1 las que se tienen que poner.
 	//las casillas ocupadas seran de la 1 a la 16 y de la 48 a la 
 	unsigned char fcolor;//b black , w white. el color de la ficha se lo pasa la propia ficha. 
